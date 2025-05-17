691466bedf2cc9ab9f744563a028b736a0915675
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-05-17 18:38:16 +0530

    added type safetly user model

--- dist/models/userModel.js
+++ dist/models/userModel.js
@@ -4,7 +4,7 @@ const mongoose_1 = require("mongoose");
 const UserSchema = new mongoose_1.Schema({
     username: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
 });
 const User = (0, mongoose_1.model)("User", UserSchema);
 exports.default = User;
