8236fd87f3bb13e9729ef6a4f1ea9d88428c680f
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-06-11 20:38:47 +0530

    added content route

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -1,9 +1,12 @@
 import express from 'express';
-import { signUp ,Login } from '../controller/userController'
+import { signUp ,Login } from '../controller/userController';
+import {AddContent} from '../controller/contentController';
+import {authMiddleware as protect} from '../middleware/authMiddleware';
 
 const router = express.Router();
 
 router.post('/signup', signUp);
 router.post('/login', Login);
+router.post('/content',protect ,AddContent);
 
 export default router;
