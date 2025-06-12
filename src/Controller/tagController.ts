496cd0094802b4581bab961ad4c66591f75b11d1
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-06-12 10:55:32 +0530

    userId get through middleware and find exist tags

--- src/Controller/tagController.ts
+++ src/Controller/tagController.ts
@@ -1,17 +1,24 @@
-import { Response ,Request } from "express";
-import Tag ,{ITag} from '../models/tagModel';
+import { Response, Request } from "express";
+import Tag from '../models/tagModel';
 
+export const AddTag = async (req: Request, res: Response): Promise<void> => {
+  const { name } = req.body;
 
-export const AddTag =async(req:Request,res:Response) :Promise <void>=>{
-    const {name} = req.body;
-    try {
-        const Tagg= new Tag({name}) 
-        await Tagg.save();
-        res.status(200).json({message:"Tag Added Succesfully",name});
-        return;
-    } catch (error) {
-       res.status(500).json({ message: 'Server error', error });
-       return;
+  try {
+    const tagExist = await Tag.findOne({ name, userId: req.user });
+
+    if (!tagExist) {
+      const newTag = new Tag({
+        name,
+        userId: req.user,
+      });
+
+      await newTag.save();
+      res.status(200).json({ message: "Tag Added Successfully", name });
+    } else {
+      res.status(400).json({ message: "Tag Already Exists" });
     }
-
-}
+  } catch (error) {
+    res.status(500).json({ message: 'Server error', error });
+  }
+};
