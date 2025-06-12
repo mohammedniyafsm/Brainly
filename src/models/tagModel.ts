2a415e91d639c0e8397f5073fffaaf89d8dac7bf
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-06-12 15:02:08 +0530

    updated Tag Model

--- src/models/tagModel.ts
+++ src/models/tagModel.ts
@@ -1,26 +1,40 @@
-import mongoose , {Document,Model,Schema,model} from "mongoose";
+import mongoose, { Document, Schema, Model } from "mongoose";
+
+interface ITagItem {
+  _id: mongoose.Types.ObjectId;
+  name: string;
+}
 
 export interface ITag extends Document {
-    _id:mongoose.Types.ObjectId,
-    name:string,
-    userId:mongoose.Types.ObjectId,
+  userId: mongoose.Types.ObjectId;
+  tags: ITagItem[];  
 }
 
 
-const TagSchema : Schema <ITag>  =new Schema ({
-    name : {
-        type : String,
-        required : true,
-        unique: true,
-        trim: true,
-    },
-    userId : {
-        type : Schema.Types.ObjectId,
-        ref : 'User',
-        required : true,
-    }
+const TagItemSchema = new Schema<ITagItem>({
+//   _id: {
+//     type: Schema.Types.ObjectId,
+//     default: () => new mongoose.Types.ObjectId()
+//   },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
 
-})
+const TagSchema: Schema<ITag> = new Schema({
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+    unique: true 
+  },
+  tags: {
+    type: [TagItemSchema],
+    default: []
+  }
+});
 
-const Tag : Model <ITag> =mongoose.model<ITag>('Tag',TagSchema);
+const Tag: Model<ITag> = mongoose.model<ITag>('Tag', TagSchema);
 export default Tag;
