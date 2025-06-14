2461b5dbfc0ea1a61f9641046adefb8214f3ee68
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-06-14 07:46:34 +0530

    testing Button component in

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,18 @@
 import './App.css'
 import { Button } from './components/ui/Button'
+import  {PlusIcon}  from './components/icons/Plus'
 
 function App() {
 
   return (
     <>
-      <Button variant='primary' text='sdcscs'/>
+      <div className="">
+        
+        <Button variant='primary' size='sm' text='share' onclick={()=>{}} startIcon={<PlusIcon size='md'/>} />
+        {/* <Button variant='secondary' size='md' text='Add Content' onclick={()=>{}}  />
+        <Button variant='secondary' size='lg' text='Add Content' onclick={()=>{}}  /> */}
+       
+      </div>
     </>
   )
 }
