689e3ceb66769cfc20f79ce5f89476ef116d20d9
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-06-14 07:45:43 +0530

    created a Button Ui Component

--- frontend/src/components/ui/Button.tsx
+++ frontend/src/components/ui/Button.tsx
@@ -1,15 +1,28 @@
 import type { ReactElement } from "react"
 
  interface ButtonProps {
-    variant : "primary" | "secodary",
+    variant : "primary" | "secondary",
+    size: "sm" | "md" | "lg",
     text: string,
     startIcon?:ReactElement,
     EndIcon?:ReactElement,
     onclick?:()=>void,
 }
 
-const variantstyle
+const sizeStyle = {
+    "sm" : "py-1 px-2",
+    "md" : "py-2 px-4",
+    "lg" : "py-6 px-8",
+}
+
+const defaultStyle = "rounded-md flex"
+
+const variantStyles = {
+    "primary" : "bg-purple-600 text-white ",
+    "secondary" : "bg-purple-300 text-purple-600",
+}
 
 export const Button =(props:ButtonProps)=>{
-    return <button></button>
+    return <button className={`${variantStyles[props.variant]} ${sizeStyle[props.size]} ${defaultStyle}`}>{props.startIcon ? <div className="pr-2">{props.startIcon }</div>:null}
+    {props.text} {props.EndIcon}</button>
 }
