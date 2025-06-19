3470e56a1883a262a5feaa984882ef87dc600a06
mohammedniyafsm <mohammedniyafpc@gmail.com>
2025-06-19 11:01:29 +0530

    added Login page

--- frontend/src/components/ui/Login.tsx
+++ frontend/src/components/ui/Login.tsx
@@ -1,11 +1,21 @@
+import { Button } from "./Button"
+import { Input } from "./CreateContentModal"
 
 
 export const Login=()=>{
     return(
-    <div className="w-screen h-screen bg-gray-100 flex items-center">
-        <div className="w-72 h-60 bg-white-100 rounded-md shadow-md ">
-            <h1>Signup</h1>
-
+    <div className="w-screen h-screen bg-gray-100 flex items-center justify-center">
+        <div className="w-92 h-80  bg-white-100 rounded-md shadow-2xl">
+            <div className="px-40 py-6">
+                <h1 className="text-2xl font-medium">Login</h1>
+            </div>
+            <div className="px-6">
+                <Input Title="username "/>
+                <Input Title="password "/>
+                <div className="px-24 py-4">
+                <Button text="Login" size="lg" variant="primary"/>
+                </div>
+            </div>
         </div>
     </div>
     )
